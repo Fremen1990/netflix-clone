a72bcf4ff594b518c58d0ebd22ece239b1d78197
Fremen1990 <fremen1990@gmail.com>
2022-05-04 09:52:18 +0200

    Jumbotron styled component finish

--- src/components/jumbotron/index.js
+++ src/components/jumbotron/index.js
@@ -1,10 +1,34 @@
-import React from 'react';
-import {Inner} from "./styles/jumbotron"
+import React from "react";
+import {
+  Container,
+  Image,
+  Inner,
+  Item,
+  Pane,
+  Subtitle,
+  Title,
+} from "./styles/jumbotron";
 
-export default function Jumbotron({}){
-return (
-	<Inner>
-		<p>Hi jumbo</p>
-	</Inner>
-)
+export default function Jumbotron({ children, direction = "row" }) {
+  return (
+    <Item direction={direction}>
+      <Inner>{children}</Inner>
+    </Item>
+  );
 }
+
+Jumbotron.Container = function JumbotronContainer({ children, ...restProps }) {
+  return <Container {...restProps}>{children}</Container>;
+};
+Jumbotron.Pane = function JumbotronPane({ children, ...restProps }) {
+  return <Pane {...restProps}>{children}</Pane>;
+};
+Jumbotron.Title = function JumbotronTitle({ children, ...restProps }) {
+  return <Title {...restProps}>{children}</Title>;
+};
+Jumbotron.Subtitle = function JumbotronSubtitle({ children, ...restProps }) {
+  return <Subtitle {...restProps}>{children}</Subtitle>;
+};
+Jumbotron.Image = function JumbotronImage({ ...restProps }) {
+  return <Image {...restProps} />;
+};
