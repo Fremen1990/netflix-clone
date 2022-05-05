9be85b55425be467df58f10009d7fa1dcfa6d95f
Fremen1990 <fremen1990@gmail.com>
2022-05-06 00:30:22 +0200

    constant-routes finished and tested.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { JumbotronContainer } from "./containers/JumbotronContainer";
 import { FooterContainer } from "./containers/FooterContainer";
 import { FaqsContainer } from "./containers/FaqsContainer";
+import * as ROUTES from "./constants/routes";
 
 export default function App() {
   return (
     <>
-      <JumbotronContainer />
-      <FaqsContainer />
-      <FooterContainer />
+      <Router>
+        <Routes>
+          <Route
+            path={ROUTES.HOME}
+            element={
+              <>
+                <JumbotronContainer />
+                <FaqsContainer />
+                <FooterContainer />
+              </>
+            }
+          />
+        </Routes>
+      </Router>
     </>
   );
 }
