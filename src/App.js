aa10aa060dd0d7e751a5c045063ec9599c28ca0d
Fremen1990 <fremen1990@gmail.com>
2022-05-09 23:54:46 +0200

    useAuthListener custom hook implemented with Firebase database and protected route for Browse page implemented as well.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-
-import * as ROUTES from "./constants/routes";
 import { Home, Browse, Signin, Signup } from "./pages";
+import * as ROUTES from "./constants/routes";
+import { ProtectedRoute } from "./helpers/routes";
+// import { useAuthListener } from "./hooks";
 
 export default function App() {
+  // const { user } = useAuthListener();
+
   return (
     <>
       <Router>
         <Routes>
-          <Route path={ROUTES.HOME} element={<Home />} />
-          <Route path={ROUTES.BROWSE} element={<Browse />} />
           <Route path={ROUTES.SIGN_IN} element={<Signin />} />
           <Route path={ROUTES.SIGN_UP} element={<Signup />} />
+
+          <Route path={ROUTES.HOME} element={<Home />} />
+          <Route
+            path={ROUTES.BROWSE}
+            element={
+              <ProtectedRoute>
+                <Browse />
+              </ProtectedRoute>
+            }
+          />
         </Routes>
       </Router>
     </>
   );
 }
-
-//TODO FIREBASE CONNECTION -> 03:10
--- src/pages/Browse.js
+++ src/pages/Browse.js
@@ -1,5 +1,14 @@
 import React from "react";
+import { useAuthListener } from "../hooks";
 
 export default function Browse() {
-  return <h1>Hello from Browse!</h1>;
+  const { user } = useAuthListener();
+  // console.log("This is USER UID:", user.uid);
+
+  return (
+    <h1>
+      Hello from Browse! Hello my DEAR
+      {user.displayName}
+    </h1>
+  );
 }
