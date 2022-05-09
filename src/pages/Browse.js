fefb7e6a31cc2a1557592e343f722d17a5ec094d
Fremen1990 <fremen1990@gmail.com>
2022-05-09 23:59:28 +0200

    Notes for Browse Page.

--- src/pages/Browse.js
+++ src/pages/Browse.js
@@ -5,6 +5,12 @@ export default function Browse() {
   const { user } = useAuthListener();
   // console.log("This is USER UID:", user.uid);
 
+  // We need the series and films, fetch them
+
+  // We need slides for them
+
+  // We need to pass it to the browse container
+
   return (
     <h1>
       Hello from Browse! Hello my DEAR
