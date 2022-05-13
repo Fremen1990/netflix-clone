53f34aa995b8905f68dcc21bafeb83e700b68846
Fremen1990 <fremen1990@gmail.com>
2022-05-13 16:52:45 +0200

    Card tests init.

---
+++ src/__tests__/components/card.test.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import { Card, Player } from "../../components";
+
+const category = "series";
+
+//TODO 08:28
